7f28a271a76f88ba35c68d99ba28fa12421ebbf3
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-08-24 21:46:07 +0200

    get carousel to work

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,16 @@
-console.log('hello')
-
 const carousel = document.getElementById('carousel');
 const movable = document.getElementsByClassName('carousel__viewport')[0]
 
-let currentActiveSlide = 0;
+const totalNumberOfSlides = movable.getElementsByTagName('img').length;
+let currentSlideIndex = 0;
 
 function slideLeft() {
-  console.log('hi from slideLeft')
-  // const first = carousel.children[0];
-  // first.classList.add('slide-left');
-  // first.addEventListener('animationend', function() {
-
-  // })
-  const images = carousel.children;
-  const widthToSide = carousel.children[0].width;
-  movable.classList.add('slide-left');
-  movable.addEventListener('ontransitionend', function() {
-    movable.classList.remove('slide-left')
-  })
+  if (currentSlideIndex === totalNumberOfSlides-1) {
+    currentSlideIndex = 0;
+    return
+  }
+    movable.style.transform = `translateX(-${currentSlideIndex+1}00%)`;
+    currentSlideIndex++;
 }
 
 const interval = setInterval(slideLeft, 4000)
