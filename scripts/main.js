bf28a478fd78043c5b51b76cef9b4ec79c5d46cb
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-08-29 20:19:35 +0200

    lint index.html, scope js, hide carousel on small viewports

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,20 @@
-const carousel = document.getElementById('carousel');
-const movable = document.getElementsByClassName('carousel__viewport')[0]
+function initCarousel(seconds) {
+  const carousel = document.getElementById('carousel');
+  const movable = carousel.getElementsByClassName('carousel__viewport')[0]
 
-const totalNumberOfSlides = movable.getElementsByTagName('img').length;
-let currentSlideIndex = 0;
+  const totalNumberOfSlides = movable.getElementsByTagName('img').length;
+  let currentSlideIndex = 0;
 
-function slideLeft() {
-  if (currentSlideIndex === totalNumberOfSlides-1) {
-    currentSlideIndex = 0;
-    return
+  function slideLeft() {
+    if (currentSlideIndex === totalNumberOfSlides-1) {
+      currentSlideIndex = 0;
+      return
+    }
+      movable.style.transform = `translateX(-${currentSlideIndex+1}00%)`;
+      currentSlideIndex++;
   }
-    movable.style.transform = `translateX(-${currentSlideIndex+1}00%)`;
-    currentSlideIndex++;
+
+const interval = setInterval(slideLeft, seconds*1000 )
 }
 
-const interval = setInterval(slideLeft, 4000)
+initCarousel(4)
