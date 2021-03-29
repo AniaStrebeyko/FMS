8f99c5ba06154aa7af44080e982c02919cc46ad4
Jakub Jan Strebeyko <strebeyko@gmx.com>
2021-03-30 00:17:18 +0200

    fix: shorten carousel
    + endure padding on mobile;

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,16 @@
 function initCarousel(seconds) {
   const carousel = document.getElementById('carousel');
-  const movable = carousel.getElementsByClassName('carousel__viewport')[0]
+  const movable = carousel.getElementsByClassName('carousel__tray')[0]
 
-  const totalNumberOfSlides = movable.getElementsByTagName('img').length;
+  const totalNumberOfSlides = 10;
+  // tworzymy divy, "obrazki" w JS, inaczej mielibyśmy fatalne powtórzenia.
+  for (let i = 0; i < 10; i++ ) {
+    div = document.createElement('div');
+    staticImgIndex = i + 1;
+    formattedIndex = staticImgIndex < 10 ?  '0' + staticImgIndex : staticImgIndex;
+    div.style.backgroundImage = `linear-gradient(rgba(255,255,255, 0.3), rgba(255,255,255,1)), url("../static/${formattedIndex}.png")`
+    movable.append(div)
+  }
   let currentSlideIndex = 0;
 
   function slideLeft() {
@@ -10,7 +18,7 @@ function initCarousel(seconds) {
       currentSlideIndex = 0;
       return
     }
-      movable.style.transform = `translateX(-${currentSlideIndex+1}00%)`;
+      movable.style.transform = `translateX(-${currentSlideIndex+1}0%)`;
       currentSlideIndex++;
   }
 
